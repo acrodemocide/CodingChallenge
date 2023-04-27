f1ca7b583339c7b1fcb66b9ab47b5993eb4e0fdb
dhowardAtmarketsharpDotcom <dhoward@marketsharp.com>
2023-04-27 13:16:53 -0600

    Added fizzbuzz challenge and tests

--- CodingChallenge/CodingChallenge.cs
+++ CodingChallenge/CodingChallenge.cs
@@ -1,4 +1,6 @@
-﻿namespace CodingChallenges
+﻿using System.Text;
+
+namespace CodingChallenges
 {
     public class CodingChallenge
     {
@@ -21,6 +23,31 @@
             {
                 return Fibbonacci(n - 1) + Fibbonacci(n - 2);
             }
+        }
+
+        public Dictionary<int, string> FizzBuzz()
+        {
+            List<int> numbers = new List<int>();
+            for(int i = 1; i < 101; i++)
+            {
+                numbers.Add(i);
+            }
+
+            Dictionary<int, string> result = new Dictionary<int, string>();
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                StringBuilder sb = new StringBuilder();
+                if (numbers[i] % 3 == 0)
+                {
+                    sb.Append("Fizz");
+                }
+                if (numbers[i] % 5 == 0)
+                {
+                    sb.Append("Buzz");
+                }
+                result.Add(numbers[i], sb.ToString());
+            }
+            return result;
         }
     }
 }
---
+++ CodingChallengeTests/FizzBuzzTests.cs
@@ -0,0 +1,63 @@
+﻿using CodingChallenges;
+
+namespace CodingChallengeTests
+{
+    [TestClass]
+    public class FizzBuzzTests
+    {
+        private CodingChallenge _codingChallenge;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _codingChallenge = new CodingChallenge();
+        }
+
+        [TestMethod]
+        public void TestSpecificNumbers()
+        {
+            var fizz = "Fizz";
+            var buzz = "Buzz";
+            var fizzBuzz = fizz + buzz;
+
+            var result = _codingChallenge.FizzBuzz();
+            Assert.IsNotNull(result);
+            Assert.AreEqual(result[3], fizz);
+            Assert.AreEqual(result[5], buzz);
+            Assert.AreEqual(result[9], fizz);
+            Assert.AreEqual(result[10], buzz);
+            Assert.AreEqual(result[15], fizzBuzz);
+            Assert.AreEqual(result[30], fizzBuzz);
+        }
+
+        [TestMethod]
+        public void TestGeneralNumbers()
+        {
+            var fizz = "Fizz";
+            var buzz = "Buzz";
+            var fizzBuzz = fizz + buzz;
+
+            var result = _codingChallenge.FizzBuzz();
+
+            for(int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Assert.AreEqual(result[i], fizzBuzz);
+                }
+                else if (i % 3 == 0)
+                {
+                    Assert.AreEqual(result[i], fizz);
+                }
+                else if (i % 5 == 0)
+                {
+                    Assert.AreEqual(result[i], buzz);
+                }
+                else
+                {
+                    Assert.AreEqual(result[i], string.Empty);
+                }
+            }
+        }
+    }
+}
