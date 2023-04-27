771425e472353f9687a96c68d802e8def080c208
dhowardAtmarketsharpDotcom <dhoward@marketsharp.com>
2023-04-27 11:54:36 -0600

    First commit

---
+++ CodingChallenge/CodingChallenge.cs
@@ -0,0 +1,26 @@
+﻿namespace CodingChallenges
+{
+    public class CodingChallenge
+    {
+        public int Fibbonacci(int n)
+        {
+            if (n < 0)
+            {
+                throw new Exception("Cannot accept negative numbers");
+            }
+
+            if (n == 0)
+            {
+                return 0;
+            }
+            else if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return Fibbonacci(n - 1) + Fibbonacci(n - 2);
+            }
+        }
+    }
+}
---
+++ CodingChallengeTests/FibbonacciTests.cs
@@ -0,0 +1,87 @@
+using CodingChallenges;
+
+namespace CodingChallengeTests
+{
+    [TestClass]
+    public class FibbonacciTests
+    {
+        private CodingChallenge _codingChallenge;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _codingChallenge = new CodingChallenge();
+        }
+
+        [TestMethod]
+        public void TestNegativeInput()
+        {
+            var input = -1;
+            var expected = "Cannot accept negative numbers";
+
+            try
+            {
+                var actual = _codingChallenge.Fibbonacci(input);
+                Assert.Fail("Should not pass here");
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual(ex.Message, expected);
+            }
+        }
+
+        [TestMethod]
+        public void TestZeroInput()
+        {
+            var input = 0;
+            var expected = 0;
+            var actual = _codingChallenge.Fibbonacci(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestFirstElement()
+        {
+            var input = 1;
+            var expected = 1;
+            var actual = _codingChallenge.Fibbonacci(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestSecondElement()
+        {
+            var input = 2;
+            var expected = 1;
+            var actual = _codingChallenge.Fibbonacci(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestThirdElement()
+        {
+            var input = 3;
+            var expected = 2;
+            var actual = _codingChallenge.Fibbonacci(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestFifteenthElement()
+        {
+            var input = 15;
+            var expected = 610;
+            var actual = _codingChallenge.Fibbonacci(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestFortyFifthElement()
+        {
+            var input = 45;
+            var expected = 1134903170;
+            var actual = _codingChallenge.Fibbonacci(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
