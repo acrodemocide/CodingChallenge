62fd94c34f69d5de8600cb9774979f4da25de994
dhowardAtmarketsharpDotcom <dhoward@marketsharp.com>
2023-06-05 11:54:20 -0600

    Updated tests for Arabic to Roman numeral conversion

--- CodingChallengeTests/RomanNumeralsTests.cs
+++ CodingChallengeTests/RomanNumeralsTests.cs
@@ -69,6 +69,216 @@ namespace CodingChallengeTests
         }
 
         [TestMethod]
+        public void Test2()
+        {
+            var actual = _codingChallenge.ConvertArabicToRomanNumerals(2);
+            var expected = "II";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test3()
+        {
+            var actual = _codingChallenge.ConvertArabicToRomanNumerals(3);
+            var expected = "III";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test4()
+        {
+            var actual = _codingChallenge.ConvertArabicToRomanNumerals(4);
+            var expected = "IV";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test5()
+        {
+            var actual = _codingChallenge.ConvertArabicToRomanNumerals(5);
+            var expected = "V";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test6()
+        {
+            var actual = _codingChallenge.ConvertArabicToRomanNumerals(6);
+            var expected = "VI";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test7()
+        {
+            var actual = _codingChallenge.ConvertArabicToRomanNumerals(7);
+            var expected = "VII";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test8()
+        {
+            var actual = _codingChallenge.ConvertArabicToRomanNumerals(8);
+            var expected = "VIII";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test9()
+        {
+            var actual = _codingChallenge.ConvertArabicToRomanNumerals(9);
+            var expected = "IX";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test10()
+        {
+            var actual = _codingChallenge.ConvertArabicToRomanNumerals(10);
+            var expected = "X";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test11()
+        {
+            var actual = _codingChallenge.ConvertArabicToRomanNumerals(11);
+            var expected = "XI";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test30()
+        {
+            var actual = _codingChallenge.ConvertArabicToRomanNumerals(30);
+            var expected = "XXX";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test40()
+        {
+            var actual = _codingChallenge.ConvertArabicToRomanNumerals(40);
+            var expected = "XL";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test50()
+        {
+            var actual = _codingChallenge.ConvertArabicToRomanNumerals(50);
+            var expected = "L";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test60()
+        {
+            var actual = _codingChallenge.ConvertArabicToRomanNumerals(60);
+            var expected = "LX";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test80()
+        {
+            var actual = _codingChallenge.ConvertArabicToRomanNumerals(80);
+            var expected = "LXXX";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test90()
+        {
+            var actual = _codingChallenge.ConvertArabicToRomanNumerals(90);
+            var expected = "XC";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test100()
+        {
+            var actual = _codingChallenge.ConvertArabicToRomanNumerals(100);
+            var expected = "C";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test110()
+        {
+            var actual = _codingChallenge.ConvertArabicToRomanNumerals(110);
+            var expected = "CX";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test300()
+        {
+            var actual = _codingChallenge.ConvertArabicToRomanNumerals(300);
+            var expected = "CCC";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test400()
+        {
+            var actual = _codingChallenge.ConvertArabicToRomanNumerals(400);
+            var expected = "CD";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test500()
+        {
+            var actual = _codingChallenge.ConvertArabicToRomanNumerals(500);
+            var expected = "D";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test600()
+        {
+            var actual = _codingChallenge.ConvertArabicToRomanNumerals(600);
+            var expected = "DC";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test800()
+        {
+            var actual = _codingChallenge.ConvertArabicToRomanNumerals(800);
+            var expected = "DCCC";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test900()
+        {
+            var actual = _codingChallenge.ConvertArabicToRomanNumerals(900);
+            var expected = "CM";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test1000()
+        {
+            var actual = _codingChallenge.ConvertArabicToRomanNumerals(1000);
+            var expected = "M";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test1100()
+        {
+            var actual = _codingChallenge.ConvertArabicToRomanNumerals(1100);
+            var expected = "MC";
+            Assert.AreEqual(expected, actual);
+        }
+
+
+
+        [TestMethod]
         public void Test1666()
         {
             var actual = _codingChallenge.ConvertArabicToRomanNumerals(1666);
@@ -81,6 +291,14 @@ namespace CodingChallengeTests
         {
             var actual = _codingChallenge.ConvertArabicToRomanNumerals(1990);
             var expected = "MCMXC";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test528()
+        {
+            var actual = _codingChallenge.ConvertArabicToRomanNumerals(528);
+            var expected = "DXXVIII";
             Assert.AreEqual(expected, actual);
         }
     }
